8140f95077007a0038fb157e191de39f94ab14e0
Hariharan <92014041+Harije@users.noreply.github.com>
2024-01-04 17:47:05 +0530

    Delete cognifyz/level 3/L3 task 1 directory

--- cognifyz/level 3/L3 task 1/script.js
+++
@@ -1,9 +0,0 @@
-function showImage(imageSrc, description) {
-  const mainImg = document.getElementById('mainImg');
-  const imageText = document.getElementById('imageText');
-
-  mainImg.src = imageSrc;
-  imageText.innerText = description;
-}
-
-  
